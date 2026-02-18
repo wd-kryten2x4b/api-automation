12d0d1b9eae978ec27c7612972b700835cca4274
William Devitt <william@devcommone.co.uk>
2026-02-18 09:28:22 +0100

    Update brands.hooks.spec.ts
    Updated the file content to reflect changes made because of Video 55  - before all hooks exercise

--- specs/brands.hooks.spec.ts
+++ specs/brands.hooks.spec.ts
@@ -1,34 +1,134 @@
-import { generatePrime } from 'node:crypto';
-import { clearScreenDown } from 'node:readline';
 import * as supertest from 'supertest';
-//import { describe, it } from 'node:test';
 const request = supertest('https://practice-react.sdetunicorns.com/api/test');
 
-describe('Brands Negative Test', () => {
+describe('Brands API Test Cases', () => {
     let newBrandId: any;
+
+  describe('Fetch brands', () => {
+    it('GET /brands', async () => {
+      const res = await request.get('/brands');
+      expect(res.statusCode).toEqual(200);
+      expect(res.body.length).toBeGreaterThan(1);
+      expect(Object.keys(res.body[0])).toEqual(['_id', 'name']);
+    });
+  });
     
-describe('Brand Schema Validation Test',() => {
-it('POST an existing brand /brands', async () => {
+describe('Create Brands',() => {
+  //new variable
+  let postBrand:any;
+  //set the Data body
+  const bNumber = Math.floor(Math.random() * 10000)
+  const data = {
+       name: 'Test Brand Samsung ' + bNumber,
+       description: 'Test Brand Samsung brand'
+      };
+  //add a before all hook here
+    beforeAll(async () => {
+        postBrand = await request
+        .post('/brands')
+        .send(data);
+    })
+it('POST /brands', async () => {
     
-        const bNumber = Math.floor(Math.random() * 10000)
+      //expect result code to be 200 (good)
+      expect(postBrand.statusCode).toEqual(200);
+      //expect name to be A Plus 5694'
+      expect(postBrand.body.name).toEqual(data.name); //use any other name value as a negative test
+      //expect the body to have createdAt filed
+      expect(postBrand.body).toHaveProperty('createdAt');      //expect result code to be 200 (good)
+        });
+
+    it('Schema Verification - Name is mandatory field', async () => {
+    
+        const data = {
+        name: '',
+        description: 'Test Brand Samsung brand'
+      }  
+      const res = await request.post('/brands')
+      .send(data);  
+
+      //expect result code to be 200 (good)
+      expect(res.statusCode).toEqual(422);
+      //expect name to be A Plus 5694'
+      expect(res.body.error).toEqual('Name is required');
+        });     
+
+      it('Schema Verification - Name minimum length >1', async () => {
+    
+        const data = {
+        name: 'A',
+        description: 'Test Brand Samsung brand'
+      }  
+      const res = await request.post('/brands')
+      .send(data);  
+
+      //expect result code to be 200 (good)
+      expect(res.statusCode).toEqual(422);
+      //expect name to be A Plus 5694'
+      expect(res.body.error).toEqual('Brand name is too short');
+        });
+
+it('Business Logig - brands name >30 chars', async () => {
+        const data = {
+        name: postBrand.name + 'updated to be greater than 30',
+      };
+      const res = await request.put('/brands/' + postBrand._id)
+      .send(data); 
+      //expect result code to be 422 (error)
+      expect(res.statusCode).toEqual(422);
+      //expect name to be A Plus 5694'
+      expect(res.body.error).toEqual('Brand name is too long'); //use any other name value as a negative test
+         });        
+
+it('Business Logic - Duplicate brand entries not allowed', async () => {
+      //1st request is created by the before all hook
+      
+      //2nd Request
+       const res2 = await request.post('/brands')
+      .send(data);
+           
+      //First request assertions
+      //expect result code to be 200 (good)
+      expect(postBrand.statusCode).toEqual(200);
+      //expect name to be A Plus 5694'
+      expect(postBrand.body.name).toEqual(data.name); //use any other name value as a negative test
+      //expect the body to have createdAt filed
+      expect(postBrand.body).toHaveProperty('createdAt');
+
+      //2nd request assertions
+      expect(res2.statusCode).toEqual(422);
+      //expect the body to have createdAt filed
+      expect(res2.body.error).toEqual(data.name +' already exists');
+
+      //newBrandId = res.body;
+        });        
+    }); 
+
+  describe('Fetch Individual Brand',() => {
+    //new vairable specifically for GET
+    let postBrand: any;
+    //add a before all hook to create a new brand so that the test can be run independantly
+    beforeAll(async () => {
+       const bNumber = Math.floor(Math.random() * 10000)
         const data = {
         name: 'Test Brand Samsung ' + bNumber,
         description: 'Test Brand Samsung brand'
       }
+      postBrand = await request
+        .post('/brands')
+        .send(data);
+    });
 
-      const res = await request.post('/brands')
-      .send(data);  
+    it('GET /brands/:id', async () => { 
+      const res = await request.get('/brands/' + postBrand.body._id);  
       //expect result code to be 200 (good)
-      expect(res.statusCode).toEqual(200);
-      //expect name to be A Plus 5694'
-      expect(res.body.name).toEqual(data.name); //use any other name value as a negative test
-      //expect the body to have createdAt filed
-      expect(res.body).toHaveProperty('createdAt');      //expect result code to be 200 (good)
-      expect(res.statusCode).toEqual(200);
+        expect(res.statusCode).toBe(200);
+      //expect name to be the  same as the one created'
+        expect(res.body.name).toEqual(postBrand.body.name); //use any other name value as a negative test
+        //newBrandId = res.body;  
+         });
 
-      newBrandId = res.body;
-        });
-    it('Business Logic - Brand doesnt exist', async () => {
+        it('Business Logic - Brand doesnt exist', async () => {
     
       const res = await request.get('/brands/69930f093a8ec410bfeece88');  
 
@@ -38,28 +138,33 @@ it('POST an existing brand /brands', async () => {
       expect(res.body.error).toEqual('Brand not found.');
         }); 
 
-    }); 
-
-  describe('Fetch a new brand Test',() => {
-    it('GET /brands/:id', async () => {
-
-      const res = await request.get('/brands/' + newBrandId._id);  
-      //expect result code to be 200 (good)
-        expect(res.statusCode).toBe(200);
-      //expect name to be the  same as the one created'
-        expect(res.body.name).toEqual(newBrandId.name); //use any other name value as a negative test
-        //newBrandId = res.body;  
-         });
-
     });
 
-    describe('PUT Schema & Business Logic validation',() => {
-      it('PUT brands fail on name >30 chars', async () => {
-        const data = {
-        name: newBrandId.name + 'updated to be greater than 30',
+  describe('Update Brands',() => {
+
+  //new variable
+  let createBrand:any;
+  //set the Data body
+  const bNumber = Math.floor(Math.random() * 10000)
+  const data = {
+       name: 'Test Brand Samsung ' + bNumber,
+       description: 'Test Brand Samsung brand'
       };
-      const res = await request.put('/brands/' + newBrandId._id)
-      .send(data); 
+  //add a before all hook here
+    beforeAll(async () => {
+        createBrand = await request
+        .post('/brands')
+        .send(data);
+    });
+
+      it('PUT brands fail on name >30 chars', async () => {
+        //Send the PUT Request
+        const data = {
+          name: createBrand.body.name + ' updated to be greater than 30',
+        };
+        const res = await request
+        .put('/brands/' + createBrand.body._id)
+        .send(data);
       //expect result code to be 422 (error)
       expect(res.statusCode).toEqual(422);
       //expect name to be A Plus 5694'
@@ -67,14 +172,14 @@ it('POST an existing brand /brands', async () => {
          });
     
         it('PUT brands description must be string', async () => {
-        
+      
         const numericName = Math.floor(Math.random() * 10000)
-          const data = {
-        name: newBrandId.name,
+        const data = {
+        name: createBrand.body.name,
         description: numericName
-      };
-      const res = await request.put('/brands/' + newBrandId._id)
-      .send(data);  
+      };      
+      const res = await request.put('/brands/' + createBrand.body._id)
+      .send(data);
       //expect result code to be 422 (error)
       expect(res.statusCode).toEqual(422);
       //expect name to be A Plus 5694'
@@ -97,11 +202,29 @@ it('POST an existing brand /brands', async () => {
         
     });
 
-   describe('DELETE a new brand Test',() => {
-      it('DELETE brands', async () => {
+   describe('DELETE Brands',() => {
+    //Set the before all hook for delete
+  //new variable
+  let deleteBrand:any;
+  //set the Data body
+  const bNumber = Math.floor(Math.random() * 10000)
+  const data = {
+       name: 'Test Brand Samsung ' + bNumber,
+       description: 'Test Brand Samsung brand'
+      };
+  //add a before all hook here
+    beforeAll(async () => {
+        deleteBrand = await request
+        .post('/brands')
+        .send(data);
+    });
+
+
+
+    it('DELETE brands', async () => {
 
       //Now delete the same brand
-      const res = await request.delete('/brands/' + newBrandId._id);
+      const res = await request.delete('/brands/' + deleteBrand.body._id);
 
       //check the status code
       expect(res.statusCode).toEqual(200)
@@ -123,5 +246,4 @@ it('POST an existing brand /brands', async () => {
          });          
          
     });
-
 })
